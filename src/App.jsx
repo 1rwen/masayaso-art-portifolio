4d2556bef691a630d47864f1b9345351db77fe87
randall-nguyen <117537259+1rwen@users.noreply.github.com>
2024-12-16 02:25:19 -0800

    fixing nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -34,18 +34,22 @@ const PageWrapper = ({ children }) => (
 // Main App Component
 function App() {
   return (
-    <div className="bg-babyBlue min-h-screen flex flex-col">
-      <Router>
-        <div className="fixed top:0 w-full h-auto z-50">
-          <Navbar />
-        </div>
+    <div className="bg-babyBlue min-h-screen">
+      {/* Fixed Navbar */}
+      <div className="fixed top-0 left-0 w-full z-50 bg-white">
+        <Navbar />
+      </div>
 
-        {/* Page transition wrapper */}
-        <PageTransitionWrapper />
-      </Router>
+      {/* Content Wrapper */}
+      <div className="pt-[64px]">
+        <Router>
+          <PageTransitionWrapper />
+        </Router>
+      </div>
     </div>
   );
 }
+
 
 // PageTransitionWrapper Component
 function PageTransitionWrapper() {
