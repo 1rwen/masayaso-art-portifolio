feaeeed31405cc43ce129c4101f8321a8570ca17
randall-nguyen <117537259+1rwen@users.noreply.github.com>
2024-12-16 02:00:42 -0800

    changes for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/masayaso-art-portifolio/", // This matches the homepage subpath
 });
