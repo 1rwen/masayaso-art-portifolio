abfbe5c6df7b0b2f004f78b39bcc047ed4584e77
randall-nguyen <117537259+1rwen@users.noreply.github.com>
2024-12-31 15:11:10 -0800

    testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 const Navbar = () => {
   return (
-    <nav className="bg-red-400 flex items-center justify-center text-3xl text-white h-12 w-full z-50">
-      <span className="leading-tight">𓆝 𓆟 𓆞 𓆝 𓆟</span>
+    <nav className="bg-royalBlue flex items-center justify-center text-3xl text-white h-12 lg:h-8 z-50 lg:z-100">
+      <span className="-translate-y-2">𓆝 𓆟 𓆞 𓆝 𓆟 </span>
     </nav>
   );
 };
 
 export default Navbar;
+
