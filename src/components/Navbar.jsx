7f988bc38020b1a5a675d6cb074c487b7d3f7cd5
randall-nguyen <117537259+1rwen@users.noreply.github.com>
2024-12-31 18:41:23 -0800

    changed index.html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="bg-yellow-400 flex items-center justify-center text-2xl xl:text-3xl text-white h-8 xl:h-8 z-20 xl:z-100">
+    <nav className="bg-royalBlue flex items-center justify-center text-2xl xl:text-3xl text-white h-8 xl:h-8 z-20 xl:z-100">
       <span className="-translate-y xl:-translate-y-2">𓆝 𓆟 𓆞 𓆝 𓆟 </span>
     </nav>
   );
