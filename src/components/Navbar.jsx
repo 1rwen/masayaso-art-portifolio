1237ba42af3871e380d760a2e5071bc4cbbe0e2d
randall-nguyen <117537259+1rwen@users.noreply.github.com>
2024-12-31 18:30:53 -0800

    fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="bg-royalBlue flex items-center justify-center text-3xl text-white h-12 lg:h-8 z-50 lg:z-100">
+    <nav className="bg-red-400 flex items-center justify-center text-3xl text-white h-166 lg:h-8 z-50 lg:z-100">
       <span className="-translate-y-2">𓆝 𓆟 𓆞 𓆝 𓆟 </span>
     </nav>
   );
