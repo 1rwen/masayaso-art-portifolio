6f55c4b78bd61cf4f8fce4c75dbd52af38ae2db3
randall-nguyen <117537259+1rwen@users.noreply.github.com>
2025-02-19 18:35:03 -0800

    push

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className="bg-babyBlue min-h-screen flex flex-col">
       <Router>
-        <div className="fixed top-0 left-0 w-full h-16 flex flex-wrap z-50">
+        <div className="fixed w-full h-auto z-50">
           <Navbar />
         </div>
 
